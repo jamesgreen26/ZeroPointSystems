7bdbb6e795c2b5df5a678caa2e6ed65548bcc9d1
James Green <jgreenop22@gmail.com>
2025-06-10 17:02:36 -0700

    fix server crash

--- src/main/java/g_mungus/zps/client/ClientSetup.java
+++ src/main/java/g_mungus/zps/client/ClientSetup.java
@@ -4,12 +4,13 @@ import g_mungus.zps.ZPSMod;
 import g_mungus.zps.client.renderer.OctoMountingRenderer;
 import g_mungus.zps.entity.ModEntities;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = ZPSMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ZPSMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientSetup {
     @SubscribeEvent
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {

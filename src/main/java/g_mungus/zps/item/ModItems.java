a5e96db2d929e441794dcc3edc09cd6ba0095128
James Green <jgreenop22@gmail.com>
2025-11-30 15:32:04 -0800

    reorder creative tab

--- src/main/java/g_mungus/zps/item/ModItems.java
+++ src/main/java/g_mungus/zps/item/ModItems.java
@@ -15,11 +15,11 @@ public class ModItems {
     public static final RegistryObject<Item> OCTO_CONTROLLER = ITEMS.register("octo_controller",
             () -> new BlockItem(ModBlocks.OCTO_CONTROLLER.get(), new Item.Properties()));
 
-    public static final RegistryObject<Item> DENSE_CABLES = ITEMS.register("dense_cables",
-        () -> new BlockItem(ModBlocks.DENSE_CABLES.get(), new Item.Properties()));
-
     public static final RegistryObject<Item> DENSE_CABLE_SEPARATOR = ITEMS.register("dense_cable_separator",
             () -> new BlockItem(ModBlocks.DENSE_CABLE_SEPARATOR.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> CABLE_INSULATION = ITEMS.register("cable_insulation",
+            () -> new BlockItem(ModBlocks.CABLE_INSULATION.get(), new Item.Properties()));
 
     public static final RegistryObject<Item> STEPUP_TRANSFORMER = ITEMS.register("stepup_transformer",
             () -> new BlockItem(ModBlocks.STEPUP_TRANSFORMER.get(), new Item.Properties()));
@@ -30,9 +30,9 @@ public class ModItems {
     public static final RegistryObject<Item> REDSTONE_CONVERTER = ITEMS.register("redstone_converter",
             () -> new BlockItem(ModBlocks.REDSTONE_CONVERTER.get(), new Item.Properties()));
 
+    public static final RegistryObject<Item> DENSE_CABLES = ITEMS.register("dense_cables",
+            () -> new BlockItem(ModBlocks.DENSE_CABLES.get(), new Item.Properties()));
+
     public static final RegistryObject<Item> CABLE = ITEMS.register("cable",
             () -> new BlockItem(ModBlocks.CABLE.get(), new Item.Properties()));
-
-    public static final RegistryObject<Item> CABLE_INSULATION = ITEMS.register("cable_insulation",
-            () -> new BlockItem(ModBlocks.CABLE_INSULATION.get(), new Item.Properties()));
 } 

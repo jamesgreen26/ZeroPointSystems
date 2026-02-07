9e884f36541a4721040d1de6f7e3f4c4665533b1
James Green <jgreenop22@gmail.com>
2026-02-06 22:40:54 -0800

    tidy up

--- src/main/java/g_mungus/zps/commands/ZPSCommands.java
+++ src/main/java/g_mungus/zps/commands/ZPSCommands.java
@@ -1,6 +1,7 @@
 package g_mungus.zps.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
@@ -17,10 +18,14 @@ public class ZPSCommands {
     public static void onRegisterCommandsEvent(RegisterCommandsEvent event) {
         CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
 
+        zpsScript(dispatcher, SetRedstoneCommand.COMMAND);
+    }
+
+    private static void zpsScript(CommandDispatcher<CommandSourceStack> dispatcher, CommandNode<CommandSourceStack> command) {
         dispatcher.register(
                 Commands.literal(PREFIX)
                         .requires(src -> src.hasPermission(2))
                         .then(Commands.argument("position", BlockPosArgument.blockPos())
-                        .then(SetRedstoneCommand.COMMAND).build()));
+                        .then(command).build()));
     }
 }

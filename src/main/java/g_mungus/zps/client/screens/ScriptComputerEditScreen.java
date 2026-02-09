b3fe7e1d3371443e3868535a8c0d3963a019d7f0
James Green <jgreenop22@gmail.com>
2026-02-08 18:09:21 -0800

    Fix button offset

--- src/main/java/g_mungus/zps/client/screens/ScriptComputerEditScreen.java
+++ src/main/java/g_mungus/zps/client/screens/ScriptComputerEditScreen.java
@@ -53,10 +53,10 @@ public class ScriptComputerEditScreen extends Screen {
     @Override
     protected void init() {
         this.doneButton = this.addRenderableWidget(
-                Button.builder(CommonComponents.GUI_DONE, arg -> this.onDone()).bounds(this.width / 2 - 4 - 150, this.height / 4 + 120 + 12, 150, 20).build()
+                Button.builder(CommonComponents.GUI_DONE, arg -> this.onDone()).bounds(this.width / 2 - 150 - 2, this.height / 4 + 120 + 12, 148, 20).build()
         );
         this.cancelButton = this.addRenderableWidget(
-                Button.builder(CommonComponents.GUI_CANCEL, arg -> this.onClose()).bounds(this.width / 2 + 4, this.height / 4 + 120 + 12, 150, 20).build()
+                Button.builder(CommonComponents.GUI_CANCEL, arg -> this.onClose()).bounds(this.width / 2 + 4, this.height / 4 + 120 + 12, 148, 20).build()
         );
 
         this.commandEdit = new MultiLineEditBox(this.font, this.width / 2 - 150, 50, 300, this.height / 4 + 70, Component.translatable("advMode.command"));

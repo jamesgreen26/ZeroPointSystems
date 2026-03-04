4d2c2afbfc60aaf1f972279e4a29109e7a348837
James Green <jgreenop22@gmail.com>
2026-03-03 23:07:11 -0800

    Fix typo

--- src/main/java/g_mungus/zps/compat/create/commands/CreateScriptExecutorMappers.java
+++ src/main/java/g_mungus/zps/compat/create/commands/CreateScriptExecutorMappers.java
@@ -5,7 +5,7 @@ public class CreateScriptExecutorMappers {
         event.accept(
                 new MappingEntry("create:mechanical_bearing", "set_movement_mode", "set_rotation_mode"),
                 new MappingEntry("create:creative_motor", "set_generated_speed_in_rpm", "set_rpm"),
-                new MappingEntry("create:rotation_speed_controller", "set_target_speed_in_rpm", "set_rpm"),
+                new MappingEntry("create:rotation_speed_controller", "set_targeted_speed_in_rpm", "set_rpm"),
                 new MappingEntry("create:secondary_linear_chassis", "set_range_of_sticky_sides", "set_sticky_range"),
                 new MappingEntry("create:linear_chassis", "set_range_of_sticky_sides", "set_sticky_range"),
                 new MappingEntry("create:radial_chassis", "set_range_of_sticky_sides", "set_sticky_range"),

31fb391347d86d5290c487be61f6b27ea560fd37
James Green <jgreenop22@gmail.com>
2026-03-03 16:00:15 -0800

    Revert "Add special script parsing for display link"
    This reverts commit 203d00aeb4d3bcb6e5aa41be305c628b7309ef58.

--- src/main/java/g_mungus/zps/client/screens/components/ScriptDispatcherProvider.java
+++ src/main/java/g_mungus/zps/client/screens/components/ScriptDispatcherProvider.java
@@ -1,21 +1,16 @@
 package g_mungus.zps.client.screens.components;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import com.mojang.brigadier.tree.ArgumentCommandNode;
 import g_mungus.zps.commands.api_impl.ZPSCommands;
 import net.minecraft.client.Minecraft;
 import net.minecraft.commands.SharedSuggestionProvider;
 
 public class ScriptDispatcherProvider implements CommandDispatcherProvider {
     private final Minecraft minecraft;
-    private final boolean allowPlainText;
     private CommandDispatcher<SharedSuggestionProvider> scriptDispatcher;
 
-    public ScriptDispatcherProvider(Minecraft minecraft, boolean allowPlainText) {
+    public ScriptDispatcherProvider(Minecraft minecraft) {
         this.minecraft = minecraft;
-        this.allowPlainText = allowPlainText;
     }
 
     @Override
@@ -24,13 +19,6 @@ public class ScriptDispatcherProvider implements CommandDispatcherProvider {
             assert this.minecraft.player != null;
             CommandDispatcher<SharedSuggestionProvider> rootDispatcher = this.minecraft.player.connection.getCommands();
             scriptDispatcher = ZPSCommands.getScriptDispatcher(rootDispatcher);
-
-            if (allowPlainText) {
-                ArgumentCommandNode<SharedSuggestionProvider, String> argumentNode
-                        = RequiredArgumentBuilder.<SharedSuggestionProvider, String>argument("display_text", StringArgumentType.greedyString()).build();
-
-                scriptDispatcher.getRoot().addChild(argumentNode);
-            }
         }
         return scriptDispatcher;
     }

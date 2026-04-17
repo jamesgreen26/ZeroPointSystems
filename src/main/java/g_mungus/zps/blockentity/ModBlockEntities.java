fe39b2c836e75042cb56dacaf2304d77fcb1c114
James Green <jgreenop22@gmail.com>
2026-04-17 16:50:58 -0700

    Fix copy paste

--- src/main/java/g_mungus/zps/blockentity/ModBlockEntities.java
+++ src/main/java/g_mungus/zps/blockentity/ModBlockEntities.java
@@ -93,9 +93,9 @@ public class ModBlockEntities {
                     () -> BlockEntityType.Builder.of(ScriptTerminalBlockEntity::new,
                             ModBlocks.SCRIPT_TERMINAL.get()).build(null));
 
-    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ScriptTerminalBlockEntity>> LOUDSPEAKER =
+    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<LoudspeakerBlockEntity>> LOUDSPEAKER =
             BLOCK_ENTITIES.register("loudspeaker",
-                    () -> BlockEntityType.Builder.of(ScriptTerminalBlockEntity::new,
+                    () -> BlockEntityType.Builder.of(LoudspeakerBlockEntity::new,
                             ModBlocks.LOUDSPEAKER.get()).build(null));
 
 } 

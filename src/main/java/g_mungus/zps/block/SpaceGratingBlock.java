a2b5045762410e6d98f884b71b73507c293f88ea
James Green <jgreenop22@gmail.com>
2026-04-18 14:11:28 -0700

    Fix space grating blocking light

--- src/main/java/g_mungus/zps/block/SpaceGratingBlock.java
+++ src/main/java/g_mungus/zps/block/SpaceGratingBlock.java
@@ -5,6 +5,7 @@ import g_mungus.zps.block.cableNetwork.properties.InsulationType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -30,5 +31,13 @@ public class SpaceGratingBlock extends Block {
 
     public void spawnDestroyParticlesPublic(Level level, Player player, BlockPos pos, BlockState state) {
         super.spawnDestroyParticles(level, player, pos, state);
+    }
+
+    protected float getShadeBrightness(BlockState p_308911_, BlockGetter p_308952_, BlockPos p_308918_) {
+        return 1.0F;
+    }
+
+    protected boolean propagatesSkylightDown(BlockState p_309084_, BlockGetter p_309133_, BlockPos p_309097_) {
+        return true;
     }
 }

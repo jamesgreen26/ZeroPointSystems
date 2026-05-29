0474ee9c598a67c6876420f004d7d1f7832e6ba7
James Green <jgreenop22@gmail.com>
2026-05-28 20:53:59 -0700

    Improve robotic arm screen

--- src/main/java/g_mungus/zps/client/screens/RoboticArmScreen.java
+++ src/main/java/g_mungus/zps/client/screens/RoboticArmScreen.java
@@ -11,13 +11,20 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
 public class RoboticArmScreen extends Screen {
     private static final Component TITLE = Component.literal("Robotic Arm");
+    private static final Component ENERGY_LABEL = Component.literal("Energy");
     private static final Component RETRIEVE_LABEL = Component.literal("\"take_items\" stack size");
     private static final Component VIEW_RANGE_LABEL = Component.literal("View Range");
-    private static final Component ENERGY_LABEL = Component.literal("Energy");
+    private static final int CONTROL_WIDTH = 200;
+    private static final int CONTROL_HEIGHT = 20;
+    private static final int LABEL_TO_CONTROL_GAP = 10;
+    private static final int SECTION_GAP = 12;
+    private static final int TITLE_TO_ENERGY_LABEL_GAP = 14;
+    private static final int TOP_SECTION_Y_OFFSET = -76;
 
     private final BlockPos blockPos;
     private RetrieveAmountSlider retrieveSlider;
@@ -37,16 +44,21 @@ public class RoboticArmScreen extends Screen {
 
         int centerX = this.width / 2;
         int centerY = this.height / 2;
-        int sliderY = centerY - 10;
-        int viewRangeButtonY = centerY + 30;
-        int bottomButtonsY = centerY + 60;
+        int topSectionY = centerY + TOP_SECTION_Y_OFFSET;
+        int energyLabelY = topSectionY + TITLE_TO_ENERGY_LABEL_GAP;
+        int barY = energyLabelY + LABEL_TO_CONTROL_GAP;
+        int retrieveLabelY = barY + CONTROL_HEIGHT + SECTION_GAP;
+        int sliderY = retrieveLabelY + LABEL_TO_CONTROL_GAP;
+        int viewRangeLabelY = sliderY + CONTROL_HEIGHT + SECTION_GAP;
+        int viewRangeButtonY = viewRangeLabelY + LABEL_TO_CONTROL_GAP;
+        int bottomButtonsY = viewRangeButtonY + CONTROL_HEIGHT + SECTION_GAP;
 
-        this.retrieveSlider = this.addRenderableWidget(new RetrieveAmountSlider(centerX - 100, sliderY, 200, 20, retrieveAmount));
+        this.retrieveSlider = this.addRenderableWidget(new RetrieveAmountSlider(centerX - 100, sliderY, CONTROL_WIDTH, CONTROL_HEIGHT, retrieveAmount));
         this.viewRangeButton = this.addRenderableWidget(Button.builder(viewRangeButtonText(), button -> {
                     this.viewRange = !this.viewRange;
                     button.setMessage(viewRangeButtonText());
                 })
-                .bounds(centerX - 100, viewRangeButtonY, 200, 20)
+                .bounds(centerX - 100, viewRangeButtonY, CONTROL_WIDTH, CONTROL_HEIGHT)
                 .build());
 
         this.addRenderableWidget(Button.builder(CommonComponents.GUI_DONE, button -> {
@@ -63,14 +75,36 @@ public class RoboticArmScreen extends Screen {
     @Override
     public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
         this.renderBackground(graphics);
-        graphics.drawCenteredString(this.font, TITLE, this.width / 2, this.height / 2 - 50, 0xFFFFFF);
-        graphics.drawString(this.font, ENERGY_LABEL, this.width / 2 - 100, this.height / 2 - 42, 0xA0A0A0);
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+        int topSectionY = centerY + TOP_SECTION_Y_OFFSET;
+        int energyLabelY = topSectionY + TITLE_TO_ENERGY_LABEL_GAP;
+        int barX = centerX - 100;
+        int barY = energyLabelY + LABEL_TO_CONTROL_GAP;
+        int barWidth = CONTROL_WIDTH;
+        int barHeight = CONTROL_HEIGHT;
+        int barInnerPadding = 1;
+        int barInnerWidth = barWidth - (barInnerPadding * 2);
+        int barInnerHeight = barHeight - (barInnerPadding * 2);
+        int retrieveLabelY = barY + barHeight + SECTION_GAP;
+        int viewRangeLabelY = retrieveLabelY + CONTROL_HEIGHT + SECTION_GAP + LABEL_TO_CONTROL_GAP;
+
+        graphics.drawCenteredString(this.font, TITLE, centerX, topSectionY, 0xFFFFFF);
+        graphics.drawString(this.font, ENERGY_LABEL, centerX - 100, energyLabelY, 0xA0A0A0);
         RoboticArmBlockEntity be = getBlockEntity();
         int energyStored = be != null ? be.getEnergyStored() : 0;
         int maxEnergy = be != null ? be.getMaxEnergyStored() : RoboticArmBlockEntity.ENERGY_CAPACITY;
-        graphics.drawString(this.font, energyStored + " / " + maxEnergy + " FE", this.width / 2 - 52, this.height / 2 - 42, 0x55FF55);
-        graphics.drawString(this.font, RETRIEVE_LABEL, this.width / 2 - 100, this.height / 2 - 22, 0xA0A0A0);
-        graphics.drawString(this.font, VIEW_RANGE_LABEL, this.width / 2 - 100, this.height / 2 + 18, 0xA0A0A0);
+        float fillRatio = maxEnergy > 0 ? (float) energyStored / (float) maxEnergy : 0.0F;
+        int filledWidth = Mth.clamp(Math.round(fillRatio * barInnerWidth), 0, barInnerWidth);
+
+        graphics.fill(barX, barY, barX + barWidth, barY + barHeight, 0xFF111111);
+        graphics.fill(barX + barInnerPadding, barY + barInnerPadding, barX + barInnerPadding + barInnerWidth, barY + barInnerPadding + barInnerHeight, 0xFF2A2A2A);
+        graphics.fill(barX + barInnerPadding, barY + barInnerPadding, barX + barInnerPadding + filledWidth, barY + barInnerPadding + barInnerHeight, 0xFF194763);
+
+        String energyText = energyStored + " / " + maxEnergy + " FE";
+        graphics.drawCenteredString(this.font, energyText, centerX, barY + 6, 0xFFFFFF);
+        graphics.drawString(this.font, RETRIEVE_LABEL, centerX - 100, retrieveLabelY, 0xA0A0A0);
+        graphics.drawString(this.font, VIEW_RANGE_LABEL, centerX - 100, viewRangeLabelY, 0xA0A0A0);
         super.render(graphics, mouseX, mouseY, partialTick);
     }
 

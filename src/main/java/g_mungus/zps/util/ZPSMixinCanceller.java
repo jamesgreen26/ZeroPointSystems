ad8929af81dc4e6aaa0e4e8bb6a9e061422e40e1
James Green <jgreenop22@gmail.com>
2026-06-09 17:53:38 -0700

    Make mixin canceller more robust

--- src/main/java/g_mungus/zps/util/ZPSMixinCanceller.java
+++ src/main/java/g_mungus/zps/util/ZPSMixinCanceller.java
@@ -1,28 +1,16 @@
 package g_mungus.zps.util;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 import java.util.List;
 
 public class ZPSMixinCanceller implements MixinCanceller {
+    private static final boolean CREATE_PRESENT =
+            LoadingModList.get().getModFileById("create") != null;
+
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        return !isCreatePresent() && mixinClassName.equals("g_mungus.zps.mixin.create.GoggleInfoExtension");
-    }
-
-    // mod list is not available early enough
-    boolean isCreatePresent() {
-        boolean createPresent;
-        try {
-            Class.forName(
-                    "com.simibubi.create.Create",
-                    false,
-                    ZPSMixinCanceller.class.getClassLoader()
-            );
-            createPresent = true;
-        } catch (ClassNotFoundException e) {
-            createPresent = false;
-        }
-        return createPresent;
+        return !CREATE_PRESENT && mixinClassName.equals("g_mungus.zps.mixin.create.GoggleInfoExtension");
     }
 }

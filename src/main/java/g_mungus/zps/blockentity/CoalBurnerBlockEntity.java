184a67a7c95457723ec4bdd1f66c843f0a339da4
James Green <jgreenop22@gmail.com>
2026-09-25 00:37:32 -0700

    Coal burner -> combustion generator (accepts any fuel now)

--- src/main/java/g_mungus/zps/blockentity/CoalBurnerBlockEntity.java
+++ src/main/java/g_mungus/zps/blockentity/CoalBurnerBlockEntity.java
@@ -7,9 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.MenuProvider;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.Containers;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -81,9 +79,7 @@ public class CoalBurnerBlockEntity extends BlockEntity implements EnergyGenerato
     }
 
     public static boolean isFuel(ItemStack stack) {
-        return !stack.isEmpty()
-                && (stack.is(ItemTags.COALS) || stack.is(Items.COAL_BLOCK))
-                && ForgeHooks.getBurnTime(stack, RecipeType.SMELTING) > 0;
+        return !stack.isEmpty() && ForgeHooks.getBurnTime(stack, RecipeType.SMELTING) > 0;
     }
 
     public IItemHandler getFuelInventory() {
@@ -163,8 +159,10 @@ public class CoalBurnerBlockEntity extends BlockEntity implements EnergyGenerato
 
         totalBurnTime = ForgeHooks.getBurnTime(fuel, RecipeType.SMELTING);
         burnTime = totalBurnTime;
+        // Like the furnace, leave the container behind (lava bucket -> bucket)
+        ItemStack remainder = fuel.getCraftingRemainingItem();
         fuel.shrink(1);
-        fuelInventory.setStackInSlot(0, fuel);
+        fuelInventory.setStackInSlot(0, fuel.isEmpty() ? remainder : fuel);
         setChanged();
     }
 
